3a7a86d098f8f3b7ba19a9eb6d645752e2843b0d
lucassantosdl <lucasdsantosm@gmail.com>
2023-05-03 16:14:09 -0300

    commit

---
+++ orders.js
@@ -0,0 +1,33 @@
+const Ordres = [
+    {
+      productName: "Foldable Mini Drone",
+      productNumber: "85631",
+      paymentStatus: "Due",
+      shopping: "Pending",
+    },
+    {
+      productName: " KF102 Drone",
+      productNumber: "85744",
+      paymentStatus: "Due",
+      shopping: "Pending",
+    },
+    {
+      productName: "F11 Pro Drone",
+      productNumber: "84197",
+      paymentStatus: "Paid",
+      shopping: "Delivered",
+    },
+    {
+      productName: "GPS 4K Drone",
+      productNumber: "85963",
+      paymentStatus: "Paid",
+      shopping: "Delivered",
+    },
+    {
+      productName: "DJI Air Drone",
+      productNumber: "85952",
+      paymentStatus: "Paid",
+      shopping: "Delivered",
+    },
+    
+  ];
